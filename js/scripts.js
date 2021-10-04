e3f3a10e26870e8239c5d340b9b1587a45b71733
Mariano Lizarza <mariano.lizarza@hotmail.com>
2021-10-04 19:02:31 -0300

    Proyecto final Javascript

---
+++ js/scripts.js
@@ -0,0 +1,271 @@
+const Clickbutton = document.querySelectorAll('.button')
+const tbody = document.querySelector('.tbody')
+let carrito = []
+
+Clickbutton.forEach(btn => {
+    btn.addEventListener('click', addToCarritoItem)
+})
+
+
+function addToCarritoItem(e) {
+    const button = e.target
+    const item = button.closest('.card')
+    const itemTitle = item.querySelector('.card-title').textContent;
+    const itemPrice = item.querySelector('.precio').textContent;
+    const itemImg = item.querySelector('.card-img-top').src;
+
+    const newItem = {
+        title: itemTitle,
+        precio: itemPrice,
+        img: itemImg,
+        cantidad: 1
+    }
+
+    addItemCarrito(newItem)
+}
+
+
+function addItemCarrito(newItem) {
+
+    const alert = document.querySelector('.alert')
+
+    setTimeout(function() {
+        alert.classList.add('hide')
+    }, 2000)
+    alert.classList.remove('hide')
+
+    const InputElemnto = tbody.getElementsByClassName('input__elemento')
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].title.trim() === newItem.title.trim()) {
+            carrito[i].cantidad++;
+            const inputValue = InputElemnto[i]
+            inputValue.value++;
+            CarritoTotal()
+            return null;
+        }
+    }
+
+    carrito.push(newItem)
+
+    renderCarrito()
+}
+
+
+function renderCarrito() {
+    tbody.innerHTML = ''
+    carrito.map(item => {
+        const tr = document.createElement('tr')
+        tr.classList.add('ItemCarrito')
+        const Content = `
+    
+    <th scope="row">1</th>
+            <td class="table__productos">
+              <img src=${item.img}  alt="">
+              <h6 class="title">${item.title}</h6>
+            </td>
+            <td class="table__price"><p>${item.precio}</p></td>
+            <td class="table__cantidad">
+              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+              <button class="delete btn btn-danger">x</button>
+            </td>
+    
+    `
+        tr.innerHTML = Content;
+        tbody.append(tr)
+
+        tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+        tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+    })
+    CarritoTotal()
+}
+
+function CarritoTotal() {
+    let Total = 0;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item) => {
+        const precio = Number(item.precio.replace("$", ''))
+        Total = Total + precio * item.cantidad
+    })
+
+    itemCartTotal.innerHTML = `Total $${Total}`
+    addLocalStorage()
+}
+
+function removeItemCarrito(e) {
+    const buttonDelete = e.target
+    const tr = buttonDelete.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    for (let i = 0; i < carrito.length; i++) {
+
+        if (carrito[i].title.trim() === title.trim()) {
+            carrito.splice(i, 1)
+        }
+    }
+
+    const alert = document.querySelector('.remove')
+
+    setTimeout(function() {
+        alert.classList.add('remove')
+    }, 2000)
+    alert.classList.remove('remove')
+
+    tr.remove()
+    CarritoTotal()
+}
+
+function sumaCantidad(e) {
+    const sumaInput = e.target
+    const tr = sumaInput.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    carrito.forEach(item => {
+        if (item.title.trim() === title) {
+            sumaInput.value < 1 ? (sumaInput.value = 1) : sumaInput.value;
+            item.cantidad = sumaInput.value;
+            CarritoTotal()
+        }
+    })
+}
+
+
+function addLocalStorage() {
+    sessionStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+window.onload = function() {
+    const storage = JSON.parse(sessionStorage.getItem('carrito'));
+    if (storage) {
+        carrito = storage;
+        renderCarrito()
+    }
+}
+
+
+
+
+//TARJETA
+
+const tarjeta = document.querySelector('#tarjeta'),
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+    formulario = document.querySelector('#formulario-tarjeta'),
+    numeroTarjeta = document.querySelector('#tarjeta .numero'),
+    nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+    logoMarca = document.querySelector('#logo-marca'),
+    firma = document.querySelector('#tarjeta .firma p'),
+    mesExpiracion = document.querySelector('#tarjeta .mes'),
+    yearExpiracion = document.querySelector('#tarjeta .year');
+ccv = document.querySelector('#tarjeta .ccv');
+
+// * Volteamos la tarjeta para mostrar el frente.
+const mostrarFrente = () => {
+    if (tarjeta.classList.contains('active')) {
+        tarjeta.classList.remove('active');
+    }
+}
+
+// * Rotacion de la tarjeta
+tarjeta.addEventListener('click', () => {
+    tarjeta.classList.toggle('active');
+});
+
+// * Boton de abrir formulario
+btnAbrirFormulario.addEventListener('click', () => {
+    btnAbrirFormulario.classList.toggle('active');
+    formulario.classList.toggle('active');
+});
+
+// * Select del mes generado dinamicamente.
+for (let i = 1; i <= 12; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion);
+}
+
+// * Select del año generado dinamicamente.
+const yearActual = new Date().getFullYear();
+for (let i = yearActual; i <= yearActual + 8; i++) {
+    let opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectYear.appendChild(opcion);
+}
+
+// * Input numero de tarjeta
+formulario.inputNumero.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formulario.inputNumero.value = valorInput
+        // Eliminamos espacios en blanco
+        .replace(/\s/g, '')
+        // Eliminar las letras
+        .replace(/\D/g, '')
+        // Ponemos espacio cada cuatro numeros
+        .replace(/([0-9]{4})/g, '$1 ')
+        // Elimina el ultimo espaciado
+        .trim();
+
+    numeroTarjeta.textContent = valorInput;
+
+    if (valorInput == '') {
+        numeroTarjeta.textContent = '#### #### #### ####';
+
+        logoMarca.innerHTML = '';
+    }
+
+    if (valorInput[0] == 4) {
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.src = "./img/visa.png";
+        logoMarca.appendChild(imagen);
+    } else if (valorInput[0] == 5) {
+        logoMarca.innerHTML = '';
+        const imagen = document.createElement('img');
+        imagen.src = "./img/mastercard.png";
+        logoMarca.appendChild(imagen);
+    }
+
+    // Volteamos la tarjeta para que el usuario vea el frente.
+    mostrarFrente();
+});
+
+// * Input nombre de tarjeta
+formulario.inputNombre.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value;
+
+    formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+    nombreTarjeta.textContent = valorInput;
+    firma.textContent = valorInput;
+
+    if (valorInput == '') {
+        nombreTarjeta.textContent = 'Yonaiker Yenderson';
+    }
+
+    mostrarFrente();
+});
+
+// * Select mes
+formulario.selectMes.addEventListener('change', (e) => {
+    mesExpiracion.textContent = e.target.value;
+    mostrarFrente();
+});
+
+// * Select Año
+formulario.selectYear.addEventListener('change', (e) => {
+    yearExpiracion.textContent = e.target.value.slice(2);
+    mostrarFrente();
+});
+
+// * CCV
+formulario.inputCCV.addEventListener('keyup', () => {
+    if (!tarjeta.classList.contains('active')) {
+        tarjeta.classList.toggle('active');
+    }
+
+    formulario.inputCCV.value = formulario.inputCCV.value
+        // Eliminar los espacios
+        .replace(/\s/g, '')
+        // Eliminar las letras
+        .replace(/\D/g, '');
+
+    ccv.textContent = formulario.inputCCV.value;
+});
